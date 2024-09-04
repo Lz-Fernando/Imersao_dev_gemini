108bc491be930c705b318803f29be5cd7f77b926
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-09-04 09:53:31 -0300

    Aula 03

---
+++ projeto_pessoal/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        nome : 'BRASIL',
+        curiosidade : 'A bandeira do Brasil é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
+    },
+    {
+        nome : 'JAPÃO',
+        curiosidade : 'A bandeira do Japão é conhecida oficialmente como Nisshōki, mas popularmente como Hinomaru, ela apresenta um design minimalista e carregado de simbolismo.',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Jap%C3%A3o'
+    },
+    {
+        nome : 'CANADÁ',
+        curiosidade : 'Na bandeira do Canadá, as onze pontas da folha de bordo não têm um significado específico, mas foram escolhidas para criar um design esteticamente agradável, no qual, o msm foi escolhido por um comitê especial, após a realização de um concurso nacional.',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Canad%C3%A1'
+    }
+];
