51ad44c311119c01595ea02594cf798b2bd26ed0
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-09-05 12:17:41 -0300

    Atualização do exemplos aleatórios na página inicial

--- projeto_pessoal/dados.js
+++ projeto_pessoal/dados.js
@@ -1,5 +1,6 @@
 let dados = [
     {
+        id : 1,
         nome : 'BRASIL',
         imagem : './assets/brazil.png',
         curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
@@ -8,6 +9,7 @@ let dados = [
         
     },
     {
+        id : 2,
         nome : 'JAPÃO',
         imagem : './assets/japao.jpg',
         curiosidade : 'A <strong>bandeira do Japão</strong> é conhecida oficialmente como Nisshōki, mas popularmente como Hinomaru, ela apresenta um design minimalista e carregado de simbolismo.',
@@ -15,6 +17,7 @@ let dados = [
         link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Jap%C3%A3o'
     },
     {
+        id : 3,
         nome : 'CANADÁ',
         imagem : './assets/canada.png',
         curiosidade : 'Na <strong>bandeira do Canadá</strong>, as onze pontas da folha de bordo não têm um significado específico, mas foram escolhidas para criar um design esteticamente agradável, no qual, o msm foi escolhido por um comitê especial, após a realização de um concurso nacional.',
@@ -22,59 +25,61 @@ let dados = [
         link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Canad%C3%A1'
     },
     {
+        id : 4,
         nome : 'ALBÂNIA',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
+        imagem : './assets/albania.png',
+        curiosidade : 'A <strong>bandeira da Albânia</strong> deriva do brasão, de desenho similar, de Gjergj Kastriot Skanderbeg, um líder albanês do século XV que esteve à frente da revolta contra o Império Otomano que resultou num breve período de independência da Albânia, entre 1443 e 1478.',
+        continente : 'Europa',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_da_Alb%C3%A2nia'
+    },
+    {
+        id : 5,
+        nome : 'ANDORRA',
+        imagem : './assets/andorra.png',
+        curiosidade : 'A <strong>bandeira de Andorra</strong> pode se considerar como uma fusão das bandeiras da França e da Catalunha, países que são os protetores da nação.',
+        continente : 'Europa',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_de_Andorra'
+    },
+    {
+        id : 6,
+        nome : 'ANGOLA',
+        imagem : './assets/angola.png',
+        curiosidade : 'Na <strong>bandeira da Angola</strong>, o vermelho simboliza o sangue derramado pelos angolanos durante as lutas pela independência, enquanto que o preto simboliza o continente africano, sua espiritualidade e os seus povos.',
+        continente : 'África',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_de_Angola'
+    },
+    {
+        id : 7,
+        nome : 'BUTÃO',
+        imagem : './assets/butao.png',
+        curiosidade : 'Na <strong>bandeira do Butão</strong>, o dragão na bandeira é branco para simbolizar a pureza. As duas cores da bandeira, divididas diagonalmente, representam o poder espiritual e temporal dentro do Butão.',
+        continente : 'Ásia',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_But%C3%A3o'
+    },
+    {
+        id : 8,
+        nome : 'BÓSNIA E HERZEGOVINA',
+        imagem : './assets/bosnia.png',
+        curiosidade : 'Na <strong>bandeira da Bósnia</strong>, as três pontas do triângulo simbolizam a teoria trinacional da Bósnia (croatas, bosníacos e sérvios). As estrelas, que representam a Europa, têm supostamente um número infinito e portanto prolongam-se para além da bandeira.',
+        continente : 'Europa',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_da_B%C3%B3snia_e_Herzegovina'
+    },
+    {
+        id : 9,
+        nome : 'QATAR',
+        imagem : './assets/catar.png',
+        curiosidade : 'Na <strong>bandeira do Qatar</strong>, dizem que a cor era vermelha, mas depois que foi pintada, foi deixada para secar na luz do sol e a cor se tornou púrpura. A cor púrpura atraiu quem fez a bandeira e ela foi modificada.',
+        continente : 'Ásia',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Catar'
         
     },
     {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
-        
-    },
-    {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
-        
-    },
-    {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
-        
-    },
-    {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
-        
-    },
-    {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
-        
-    },
-    {
-        nome : 'BRASIL',
-        imagem : './assets/brazil.png',
-        curiosidade : 'A <strong>bandeira do Brasil</strong> é uma das poucas bandeiras nacionais que possuem um lema escrito. Além disso, a disposição das estrelas é única e representa um momento histórico específico.',
-        continente : 'América do Sul',
-        link : 'https://pt.wikipedia.org/wiki/Bandeira_do_Brasil'
+        id : 10,
+        nome : 'MADAGÁSCAR',
+        imagem : './assets/madagascar.png',
+        curiosidade : 'A <strong>bandeira de Madagáscar</strong> foi adaptada pouco tempo depois da independência, em 1960, utilizando as cores que foram adaptadas pelo país ainda no século XIX.',
+        continente : 'África',
+        link : 'https://pt.wikipedia.org/wiki/Bandeira_de_Madag%C3%A1scar'
         
     }
 ];
--- projeto_pessoal/script.js
+++ projeto_pessoal/script.js
@@ -1,21 +1,26 @@
 window.onload = function () {
     let section = document.getElementById('conteudo_exemplos');
     let exemplo = '';
+    const numerosAleatorios = gerarNumerosAleatorios(5, 1, dados.length);
+    console.log(numerosAleatorios)
 
-    for (let dado of dados) {
-        exemplo += `
-            <div class="conteudo_exemplos_exemplo">
-                <img class="conteudo_exemplos_exemplo_imagem" src="${dado.imagem}" alt="Bandeira_do_${dado.nome}">
-                <div class="conteudo_exemplos_exemplo_info">
-                    <h3 class="conteudo_exemplos_exemplo_titulo">${dado.nome}</h3>
-                    <p>${dado.continente}</p>
-                    <p>${dado.curiosidade}</p>
-                    <a class="conteudo_exemplos_exemplo_descricao" href="${dado.link}" target="_blank">Saiba mais.</a>
-                </div>
-            </div>
-        `
-    }
-
+    numerosAleatorios.forEach(numero => {
+        for (let dado of dados) {
+            if (numero == dado.id){
+                exemplo += `
+                    <div class="conteudo_exemplos_exemplo">
+                        <img class="conteudo_exemplos_exemplo_imagem" src="${dado.imagem}" alt="Bandeira_do_${dado.nome}">
+                        <div class="conteudo_exemplos_exemplo_info">
+                            <h3 class="conteudo_exemplos_exemplo_titulo">${dado.nome}</h3>
+                            <p>${dado.continente}</p>
+                            <p>${dado.curiosidade}</p>
+                            <a class="conteudo_exemplos_exemplo_descricao" href="${dado.link}" target="_blank">Saiba mais.</a>
+                        </div>
+                    </div>
+                `
+            } 
+        }
+    })
     section.innerHTML = exemplo;
 }
 
@@ -41,5 +46,15 @@ function pesquisar() {
     section2.innerHTML = resultado;
     section.style.display = 'none';
     section2.style.display = 'flex';
-    
+}
+
+function gerarNumerosAleatorios (quantidade, minimo, maximo) {
+    let numeros = [];
+    while (numeros.length < quantidade) {
+        const numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+        if(!numeros.includes(numeroAleatorio)) {
+            numeros.push(numeroAleatorio);
+        }
+    }
+    return numeros;
 }
